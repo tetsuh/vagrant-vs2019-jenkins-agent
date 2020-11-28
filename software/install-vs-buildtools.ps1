924a9d6d38a8c0e471db656db2d5811c9c84fde7
tetet <tetsu.h@gmail.com>
2020-11-28 16:02:14 +0900

    Modified vs_buildtools install script to download installer.
    refer following also.
    https://github.com/rgl/jenkins-vagrant/blob/master/windows/provision-vs-build-tools.ps1

--- software/install-vs-buildtools.ps1
+++ software/install-vs-buildtools.ps1
@@ -1,3 +1,24 @@
+# This script is partially borrowed by referencing the files in the following repository.
+# https://github.com/rgl/jenkins-vagrant/blob/master/windows/provision-vs-build-tools.ps1
+
+# install the Visual Studio Build Tools 2019 16.8.
+# see https://www.visualstudio.com/downloads/
+# see https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes
+# see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2019
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/9d2147aa-7b01-4336-b665-8fe07735e5ee/c4b2212532e637ae35783660e79211e0604d0b61e54f6c7db69df30ce446bd73/vs_BuildTools.exe'
+$archiveHash = 'c4b2212532e637ae35783660e79211e0604d0b61e54f6c7db69df30ce446bd73'
+$archiveName = Split-Path $archiveUrl -Leaf
+$archivePath = "C:\vagrant\software\$archiveName"
+Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
+(New-Object Net.WebClient).DownloadFile($archiveUrl, $archivePath)
+$archiveActualHash = (Get-FileHash $archivePath -Algorithm SHA256).Hash
+if ($archiveHash -ne $archiveActualHash) {
+    throw "$archiveName downloaded from $archiveUrl to $archivePath has $archiveActualHash hash witch does not match the expected $archiveHash"
+}
+
 Write-Host "Installing Build Tools for Visual Studio 2019. This may take a while..."
 $exe = Get-ChildItem -Path C:\vagrant\software\vs_buildtools_*.exe -File | Select-Object Name
 Start-Process -FilePath "C:\vagrant\software\$($exe.Name)" -Wait -ArgumentList `
